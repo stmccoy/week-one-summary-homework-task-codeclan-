b7c620f20297dd81167c6d241e55db4e48ca77ff
crispymccoy145 <samspam19941@gmail.com>
2021-04-02 15:54:33 +0100

    first commit of answers to homework

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,1 +1,66 @@
 # WRITE YOUR FUNCTIONS HERE
+
+#function takes a pet shop dictionary as an argument and 
+#returns the name of said pet shop 
+def get_pet_shop_name(petshop_dictionary):
+    return petshop_dictionary["name"]
+
+#function takes a pet shop dictionary as an argument and
+#returns the total cash of that petshop
+def get_total_cash(petshop_dictionary):
+    return petshop_dictionary["admin"]["total_cash"]
+
+#function takes a pet shop dictionary and a number
+#as an argument and adds the number passed to the the total 
+#cash of the petshop dictionary. For losses write a 
+#negative number. 
+def add_or_remove_cash(petshop_dictionary, cash):
+    petshop_dictionary["admin"]["total_cash"] += cash
+
+#function takes a pet shop dictionary as an argument and 
+#returns total number of pets sold
+def get_pets_sold(petshop_dictionary):
+    return petshop_dictionary["admin"]["pets_sold"]
+
+#function takes in a petshop dictionary and a number as
+#an argument and increases the total pets sold, in the
+#pet shop dictionary, by that number. 
+def increase_pets_sold(petshop_dictionary, pets_sold):
+    petshop_dictionary["admin"]["pets_sold"] += pets_sold
+
+#function takes in a petshop dictionary as a function
+#and returns the total stock of pets 
+def get_stock_count(petshop_dictionary):
+    return len(petshop_dictionary["pets"])
+
+#function takes in a petshop dictionary and pet breed 
+#as an argument and returns the total number of pets
+#in stock of said breed, as an int
+def get_pets_by_breed(petshop_dictionary, breed):
+    breed_list = []
+    for b in petshop_dictionary["pets"]:
+        if b["breed"] == breed:
+            breed_list.append(b)
+    return breed_list
+
+#function takes in a petshop dictionary and pet name
+#as an argument and returns a dictionary of the pet
+#returns None if no pet is found 
+
+def find_pet_by_name(petshop_dictionary, name):
+    name_dict = {}
+    for n in petshop_dictionary["pets"]:
+        if n["name"] == name:
+            name_dict["name"] = n["name"]
+    if name_dict != {}:
+        return name_dict
+        
+
+    
+    
+
+
+
+
+
+    
--- start_point/tests/pet_shop_test.py
+++ start_point/tests/pet_shop_test.py
@@ -79,55 +79,55 @@ class TestPetShop(unittest.TestCase):
         name = get_pet_shop_name(self.cc_pet_shop)
         self.assertEqual("Camelot of Pets", name)
 
-    @unittest.skip("delete this line to run the test")
+    # @unittest.skip("delete this line to run the test")
     def test_total_cash(self):
         sum = get_total_cash(self.cc_pet_shop)
         self.assertEqual(1000, sum)
 
-    @unittest.skip("delete this line to run the test")
+    # @unittest.skip("delete this line to run the test")
     def test_add_or_remove_cash__add(self):
         add_or_remove_cash(self.cc_pet_shop,10)
         cash = get_total_cash(self.cc_pet_shop)
         self.assertEqual(1010, cash)
 
-    @unittest.skip("delete this line to run the test")
+    # @unittest.skip("delete this line to run the test")
     def test_add_or_remove_cash__remove(self):
         add_or_remove_cash(self.cc_pet_shop,-10)
         cash = get_total_cash(self.cc_pet_shop)
         self.assertEqual(990, cash)
 
-    @unittest.skip("delete this line to run the test")
+    # @unittest.skip("delete this line to run the test")
     def test_pets_sold(self):
         sold = get_pets_sold(self.cc_pet_shop)
         self.assertEqual(0, sold)
 
-    @unittest.skip("delete this line to run the test")
+    # @unittest.skip("delete this line to run the test")
     def test_increase_pets_sold(self):
         increase_pets_sold(self.cc_pet_shop,2)
         sold = get_pets_sold(self.cc_pet_shop)
         self.assertEqual(2, sold)
 
-    @unittest.skip("delete this line to run the test")
+    # @unittest.skip("delete this line to run the test")
     def test_stock_count(self):
         count = get_stock_count(self.cc_pet_shop)
         self.assertEqual(6, count)
 
-    @unittest.skip("delete this line to run the test")
+    # @unittest.skip("delete this line to run the test")
     def test_all_pets_by_breed__found(self):
         pets = get_pets_by_breed(self.cc_pet_shop, "British Shorthair")
         self.assertEqual(2, len(pets))
 
-    @unittest.skip("delete this line to run the test")
+    # @unittest.skip("delete this line to run the test")
     def test_all_pets_by_breed__not_found(self):
         pets = get_pets_by_breed(self.cc_pet_shop, "Dalmation")
         self.assertEqual(0, len(pets))
 
-    @unittest.skip("delete this line to run the test")
+    # @unittest.skip("delete this line to run the test")
     def test_find_pet_by_name__returns_pet(self):
         pet = find_pet_by_name(self.cc_pet_shop, "Arthur")
         self.assertEqual("Arthur", pet["name"])
 
-    @unittest.skip("delete this line to run the test")
+    # @unittest.skip("delete this line to run the test")
     def test_find_pet_by_name__returns_None(self):
         pet = find_pet_by_name(self.cc_pet_shop, "Fred")
         self.assertIsNone(pet)
